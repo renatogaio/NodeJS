4f204d752c62c86507186c41647f227c9357403c
Renato <renatogaio@gmail.com>
2018-11-01 00:29:24 -0300

    start new project in nodejs to create new repository

--- routes/categorias.js
+++ routes/categorias.js
@@ -2,7 +2,6 @@ const express = require('express')
 const router = express.Router()
 const controller = require('../controllers/categorias')
 
-
 router.get('/nova',controller.novaForm)
 
 router.post('/nova', controller.nova)
